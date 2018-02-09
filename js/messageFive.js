931f8f145befbadbdf79daf6793130908f94edac
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-02-09 14:42:06 -0600

    attempt at five message files

--- js/messageFive.js
+++ js/messageFive.js
@@ -1,18 +1,21 @@
 "use strict";
 
-console.log("hello first five message");
+console.log("hello fifth message");
+
+let messageFiveURL = "../lib/messageFive.json";
 
 let dataRequest = new XMLHttpRequest();
-dataRequest.addEventListener("load", dataRequestComplete);
-dataRequest.addEventListener("error", dataRequestFailed);
 
 let divPrint = document.getElementById('user-text');
 
+
+dataRequest.addEventListener("load", dataRequestComplete);
+dataRequest.addEventListener("error", dataRequestFailed);
+
 function dataRequestComplete(event) {
-    console.log("firstFive JSON Load Complete");
+    console.log("fifth message JSON Load Complete");
     var data = JSON.parse(event.target.responseText);
     console.log("data: ", data);
-    // divPrint.innerHTML = data[0].message1;
     showData(data);
     function showData(jason){
         for(var key in jason){
@@ -26,11 +29,16 @@ function dataRequestFailed(event) {
     console.log("firstFive JSON Load Failed");
   }
 
-  dataRequest.open("GET", "../lib/firstFive.json");
-  console.log("Get First Five");
+  dataRequest.open("GET", messageFiveUrl);
+  console.log("Get Fifth Message");
 
-  dataRequest.send();
+dataRequest.responseType = 'json';
+dataRequest.send();
 
 
+
+
+
+// document.getElementById
 
 // module.exports = ;

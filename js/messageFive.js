2e7cd69dc638b6334fe526ec41473d59dda6609b
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-02-09 14:54:14 -0600

    minor changes to nav2

--- js/messageFive.js
+++ js/messageFive.js
@@ -2,43 +2,42 @@
 
 console.log("hello fifth message");
 
-let messageFiveURL = "../lib/messageFive.json";
+// var messageFiveURL = "../lib/messageFive.json";
 
-let dataRequest = new XMLHttpRequest();
+// let dataRequest = new XMLHttpRequest();
 
-let divPrint = document.getElementById('user-text');
+// let divPrint = document.getElementById('user-text');
 
 
-dataRequest.addEventListener("load", dataRequestComplete);
-dataRequest.addEventListener("error", dataRequestFailed);
+// dataRequest.addEventListener("load", dataRequestComplete);
+// dataRequest.addEventListener("error", dataRequestFailed);
 
-function dataRequestComplete(event) {
-    console.log("fifth message JSON Load Complete");
-    var data = JSON.parse(event.target.responseText);
-    console.log("data: ", data);
-    showData(data);
-    function showData(jason){
-        for(var key in jason){
-            divPrint.innerHTML += `<p class ="initial">${jason[key].message}</p>`;
-        }
-    }
-
-  }
+// function dataRequestComplete(event) {
+//     console.log("fifth message JSON Load Complete");
+//     var dataFive = JSON.parse(event.target.responseText);
+//     console.log("data: ", data);
+//     showData(data);
+//     function showData(jason){
+//         for(var key in jason){
+//             divPrint.innerHTML += `<p class ="initial">${jason[key].message}</p>`;
+//         }
+//     }
+//   }
   
-function dataRequestFailed(event) {
-    console.log("firstFive JSON Load Failed");
-  }
+// function dataRequestFailed(event) {
+//     console.log("firstFive JSON Load Failed");
+//   }
 
-  dataRequest.open("GET", messageFiveUrl);
-  console.log("Get Fifth Message");
+//   dataRequest.open("GET", messageFiveURL);
+//   console.log("Get Fifth Message");
 
-dataRequest.responseType = 'json';
-dataRequest.send();
-
-
+// dataRequest.responseType = 'json';
+// dataRequest.send();
 
 
 
-// document.getElementById
 
-// module.exports = ;
+
+// // document.getElementById
+
+// // module.exports = ;

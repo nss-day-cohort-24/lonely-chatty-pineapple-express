7198a631157b7f7d1f27159d65c46477b2d6890e
MKAVARTS <mkavarts@gmail.com>
2018-02-09 12:13:52 -0600

    add/ remove update

--- js/remove.js
+++ js/remove.js
@@ -5,31 +5,39 @@ let messageCollect = messageArray.messageCollect;
 let printDiv = document.getElementById('user-text');
 
 
-document.addEventListener("click", identifyMessage);
+document.addEventListener("click", removeMessage);
 
 
-function identifyMessage (){
+function removeMessage (){
     if(event.target.className === 'remove'){
         var index = event.target.parentNode.id;
+        var element = document.getElementById(index);
+        console.log(index);
         messageCollect.forEach(function(item){
-            console.log(index);
             console.log(item.identifier);
-            if(index === item.identifer){
-                console.log(item);
+            if(item.identifier === index){
+                messageCollect.splice(item, 1);
+                element.parentNode.removeChild(element);
+                console.log(messageCollect);
             }
-        });
-        }        
+            });
     }
+}
+    
 
+// function identifyMessage(removeMessage()){
 
-
-
-// function remove(event) {
-//         if(event.target.className === 'remove'){
-//         var dadId = event.target.parentNode.id;
-//         var element = document.getElementById(dadId);
-//         element.parentNode.removeChild(element);
-//     }
 // }
+
+
+
+
+function remove(event) {
+        if(event.target.className === 'remove'){
+        var dadId = event.target.parentNode.id;
+        var element = document.getElementById(dadId);
+        element.parentNode.removeChild(element);
+    }
+}
 
   
